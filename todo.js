f40813483e44cddb76f769485056f9cc9cee3558
michael oates <moates@gmail.com>
2022-01-06 23:04:19 +1100

    todo behavior working

--- todo.js
+++ todo.js
@@ -1,13 +1,29 @@
-setTheTable();
+"use strict";
 
+let taskArray = [];
 
+function Task(title, content){
+  this.title = title;
+  this.content = content;
+  this.timeAdded = Date.now();
+};
 
-function setTheTable(){
-  document.querySelector("#addTask").addEventListener("click", createEntryFields);
+init();
+
+function init(){
+  document.querySelector("#addTask").addEventListener("click", toggleEntryFields);
+  document.querySelector("#submit").addEventListener("click", handleSubmitClick);
+  document.querySelector("#clearInput").addEventListener("click", clearInput);
+  console.log(taskArray);
+  console.log(localStorage.todoTaskArray);
+  if(localStorage.getItem("todoTaskArray") != null){
+    taskArray = JSON.parse(localStorage.getItem("todoTaskArray"));
+  }
+  console.log(taskArray);
+  drawTaskList();
 }
 
-function createEntryFields(){
-
+function toggleEntryFields(){
   if (document.querySelector("#addTask").innerHTML === "-"){
     document.querySelector("#addTask").innerHTML = "+";
   }
@@ -15,4 +31,45 @@ function createEntryFields(){
     document.querySelector("#addTask").innerHTML = "-";
   }
   document.querySelector("#taskEntry").classList.toggle("hidden");
+}
+
+function handleSubmitClick(){
+  taskArray.push(new Task(
+    document.querySelector("#titleField").value,
+    document.querySelector("#taskField").value
+  ))
+
+  localStorage.setItem("todoTaskArray", JSON.stringify(taskArray));
+  clearInput();
+  toggleEntryFields();
+  drawTaskList();
+}
+
+function clearInput(){
+  document.querySelector("#titleField").value = "";
+  document.querySelector("#taskField").value = "";
+}
+
+function drawTaskList(){
+  let buildEntry = "";
+  
+  for(let task of taskArray){
+    buildEntry += 
+    `<div class="taskItem">
+    <span class="taskTitle">
+    ${task.title}
+    </span>
+    <br />
+    ${task.content}
+    <button onclick="removeEntry(${taskArray.indexOf(task)})">x</button>
+    </div>`;
+    
+  }
+  
+  document.querySelector("#taskArray").innerHTML = buildEntry;
+}
+
+function removeEntry(entryNumber){
+  taskArray.splice(entryNumber, 1);
+  drawTaskList();
 }
